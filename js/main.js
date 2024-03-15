1cc5847f23cd37ca4aab9f6d8bef35e0e0a5b271
David <davidkang107@gmail.com>
2024-03-15 16:49:52 +0900

    3/15 업데이트

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ new Swiper('.swiper-container', {
   direction: 'horizontal',
   loop: true, 
   centeredSlides: true,
-  // autoplay: true,
-  // autoplay: {
-  //   delay: 5000 
-  // },
+  autoplay: true,
+  autoplay: {
+    delay: 5000 
+  },
   speed: 1000,
   slidesPerView: 1,
   spaceBetween: 3000,
