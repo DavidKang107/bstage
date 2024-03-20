d6bc614af4bde44abdac56235f1eff0a003ab926
David <davidkang107@gmail.com>
2024-03-20 16:45:47 +0900

    3/20 최종 업데이트

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ window.addEventListener("scroll", function() {
     } else {
         scrollButton.classList.remove("hidden"); 
     }
-
     lastScrollTop = currentScroll; 
 });
 
@@ -50,6 +49,31 @@ spyEls.forEach(function (spyEl) {
   // 라이브러리에서 지정한 문법으로 깊게 이해X
 });
 
+const spyEls_2 = document.querySelectorAll('.scroll-spy_2');
+spyEls_2.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 1 // 화면의 80% 지점에서 보여짐 여부 감시(0~1사이 지정)
+  })
+  .setClassToggle(spyEl, 'show_2') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+  // 라이브러리에서 지정한 문법으로 깊게 이해X
+});
+
+
+window.addEventListener('scroll', handleScroll);
+function handleScroll(event) {
+  // 특정 클래스 추가 여부 확인
+  if (document.querySelector('.show_2')) {
+    // y축으로 스크롤한 값
+    const scrollY = window.scrollY;
+    console.log('Scrolled Y:', scrollY);
+    const box = document.querySelectorAll('.show_2 > div:first-child')
+    box.forEach(function (item) {
+      item.style.transform = `translateY(${scrollY*0.4-2000}px)`
+    })
+  }
+};
 
 
 
