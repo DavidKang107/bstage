777a869e4f18c5c987f146eeb65b068cdcb33ccd
David <davidkang107@gmail.com>
2024-03-20 09:49:38 +0900

    3/20 중간 업데이트

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", function() {
 });
 
 window.addEventListener('scroll', function() {
-  let scrollButton = document.getElementById("scrollButton");
+  let scrollButton = document.getElementById("to-top");
 
   if (window.scrollY > 50) {
       scrollButton.classList.add("active");
@@ -39,12 +39,18 @@ new Swiper('.swiper', {
   },
 });
 
-document.querySelector('.footer__button--top').addEventListener("click", function() {
-  window.scrollTo({
-      top: 0,
-      behavior: 'smooth' 
-  });
+const spyEls = document.querySelectorAll('.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({ // 감시할 장면(Scene) 추가 및 옵션 지정
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.3 // 화면의 80% 지점에서 보여짐 여부 감시(0~1사이 지정)
+  })
+  .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+  .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+  // 라이브러리에서 지정한 문법으로 깊게 이해X
 });
+
+
 
 
 
